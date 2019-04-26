7b644a20b7d00affd61f9eb35529358d1ae56f63
xlapt <viclx1223@gmail.com>
2019-04-26 17:51:29 -0400

    4/26/2019

---
+++ generator/generator.py
@@ -0,0 +1,49 @@
+from socket import *
+import datetime
+import random
+import time
+import json
+
+def ip_addr_generator():
+    x = [str(random.randrange(1, 255)), str(random.randrange(0, 255)),
+         str(random.randrange(0, 255)), str(random.randrange(0, 255))]
+    return ".".join(x)
+
+
+def port_num_generator():
+    return str(random.randrange(1, 10000))
+
+
+def data_size_generator():
+    return str(random.randrange(1, 10000))
+
+
+def generate():
+    services = ["http", "tcp", "smtp", "pop3", "imap", "mysql", "ftp", "dns", "udp", "bolt"]
+    msg1 = [str(datetime.datetime.now())
+        , services[random.randrange(0, len(services))]
+        , port_num_generator()
+        , ip_addr_generator()
+        , "127.0.0.1"
+        , data_size_generator()]
+
+    return "/".join(msg1)
+
+
+servername = 'localhost'
+serverPort = 9999
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(("localhost", 9999))
+sock.listen(1)
+
+while True:
+    print("start")
+    conn, addr = sock.accept()
+    while True:
+        time.sleep(1)
+        msg = generate() + "\n"
+        print "sending: ", msg
+        conn.send(msg)
+
+
+
---
+++ sparksql/t.py
@@ -0,0 +1,90 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import *
+import datetime
+sc = SparkContext("local[2]", "myapp")
+sc.setLogLevel("ERROR")
+ssc = StreamingContext(sc, 30)
+ssc.checkpoint("checkpoint_TwitterApp")
+
+
+def getSparkSessionInstance(sparkConf):
+    if ("sparkSessionSingletonInstance" not in globals()):
+        globals()["sparkSessionSingletonInstance"] = SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
+            .getOrCreate()
+    return globals()["sparkSessionSingletonInstance"]
+
+
+# DataFrame operations inside your streaming program
+
+words = ssc.socketTextStream("localhost", 9999)
+
+def top_protocol_H_T(time, rdd, H, T):
+    """
+    :param H:
+    :param T:
+    :return:
+    """
+    print("\n")
+    print("========= %s =========" % str(time))
+    try:
+        # Get the singleton instance of SparkSession
+        spark = getSparkSessionInstance(rdd.context.getConf())
+
+        rowRdd = rdd.map(lambda p: p.split("/"))
+        
+        # Convert RDD[String] to RDD[Row] to DataFrame
+        rowRdd = rowRdd.map(lambda p: Row(ts=datetime.datetime.strptime(p[0], '%Y-%m-%d %H:%M:%S.%f'),
+                                          protocol=p[1], portNum=p[2], src_ip=p[3], dest_ip=p[4], data_size=p[5]))
+        wordsDataFrame = spark.createDataFrame(rowRdd)
+
+        # Creates a temporary view using the DataFrame
+        wordsDataFrame.createOrReplaceTempView("services")
+
+        q = "SELECT b.protocol from " \
+            "(select protocol, sum(data_size) as bw from services where unix_timestamp(current_timestamp()) - unix_timestamp(ts) < " + str(T) +" group by protocol) as b " \
+            "where b.bw > (select sum(data_size) from services)/"+str(H)
+
+        #x = "where unix_timestamp(current_timestamp()) - unix_timestamp(ts) < 20"
+        # q = "select ts from services where unix_timestamp(current_timestamp()) - unix_timestamp(ts) < 5"
+        # Do word count on table using SQL and print it
+        wordCountsDataFrame = spark.sql(q)
+
+        wordCountsDataFrame.show()
+    except:
+        pass
+
+
+#  List the top-k most resource intensive protocols over the last T time units.
+def top_k_protocols(time, rdd, k, T):
+    """
+
+    :param k:
+    :param T:
+    :return:
+    """
+    print("\n")
+    print("========= %s =========" % str(time))
+    try:
+        spark = getSparkSessionInstance(rdd.context.getConf())
+        rowRdd = rdd.map(lambda p: p.split("/"))
+        rowRdd = rowRdd.map(lambda p: Row(ts=datetime.datetime.strptime(p[0], '%Y-%m-%d %H:%M:%S.%f'),
+                                          protocol=p[1], portNum=p[2], src_ip=p[3], dest_ip=p[4], data_size=p[5]))
+        wordsDataFrame = spark.createDataFrame(rowRdd)
+        wordsDataFrame.createOrReplaceTempView("services")
+
+        q = "SELECT b.protocol, b.t from " \
+            "(select protocol, count(protocol) as t from services where unix_timestamp(current_timestamp()) - unix_timestamp(ts) < " + str(T) +" group by protocol) as b order by b.t desc limit " + str(k)
+
+        wordCountsDataFrame = spark.sql(q)
+        wordCountsDataFrame.show()
+    except:
+        pass
+
+
+words.foreachRDD(lambda x: top_k_protocols(datetime.datetime.now(),x,20,5))
+ssc.start()
+ssc.awaitTermination()
+
