e344b730ce55531eab90e96486d5d3717d7da04f
halflkaka <cs3736@columbia.edu>
2019-05-04 15:45:48 -0400

    05/04/2019 generate same message

--- generator/generator.py
+++ generator/generator.py
@@ -20,12 +20,20 @@ def data_size_generator():
 
 def generate():
     services = ["http", "tcp", "smtp", "pop3", "imap", "mysql", "ftp", "dns", "udp", "bolt"]
-    msg1 = [str(datetime.datetime.now())
-        , services[random.randrange(0, len(services))]
-        , port_num_generator()
-        , ip_addr_generator()
-        , "127.0.0.1"
-        , data_size_generator()]
+    if random.randrange(0,5) == 1:
+        msg1 = [str(datetime.datetime.now())
+            , services[1]
+            , "3000"
+            , "0.0.0.1"
+            , "127.0.0.1"
+            , "1000"]
+    else:
+        msg1 = [str(datetime.datetime.now())
+            , services[random.randrange(0, len(services))]
+            , port_num_generator()
+            , ip_addr_generator()
+            , "127.0.0.1"
+            , data_size_generator()]
 
     return "/".join(msg1)
 
