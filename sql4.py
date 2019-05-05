f147ca7fc06b131c582a633624bf39dae4570052
xlapt <viclx1223@gmail.com>
2019-05-05 14:48:52 -0400

    5/5/2019

---
+++ sql4.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3.7
+
+from sqlfuncs.sql import *
+sc = SparkContext("local[10]", "myapp")
+sc.setLogLevel("ERROR")
+ssc = StreamingContext(sc, 2)
+ssc.checkpoint("checkpoint_App")
+
+words = ssc.socketTextStream("localhost", 9999)
+words1 = words.window(windowDuration=16, slideDuration=2)
+words2 = words.window(windowDuration=16, slideDuration=2)
+words1.foreachRDD(lambda time, x: top_k_ip(time, x, 15, 15))
+words2.foreachRDD(lambda time, x: top_k_protocols(time, x, 15, 15))
+ssc.start()
+ssc.awaitTermination()
