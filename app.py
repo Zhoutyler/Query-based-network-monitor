f62d5a3e14c0d05d84a072ba23aeff337556aad9
xlapt <viclx1223@gmail.com>
2019-05-06 19:58:10 -0400

    5/6/2019

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ r = redis.StrictRedis(
 
 
 def get_res(ts, kind, H, T):
-    t = ts.strftime("%s")
-    key = "_".join([t, kind, H, T])
+    key = "_".join([str(ts), kind, H, T])
     res = r.hgetall(key)
     return res
 
@@ -41,24 +40,16 @@ def handle_request(query_id, H, T):
     # 5 -> top_k_ip_H
     # 6 -> ip_x_more_than_stddev
     '''
-    res = None
     ts = datetime.datetime.now()
+    t = int(ts.strftime("%s"))
     try:
-        if query_id == "1":
-            res = get_res(ts, query_id, H, T)
-        elif query_id == "2":
-            res = get_res(ts, query_id, H, T)
-        elif query_id == "3":
-            res = get_res(ts, query_id, H, T)
-        elif query_id == "4":
-            res = get_res(ts, query_id, H, T)
-        elif query_id == "5":
-            res = get_res(ts, query_id, H, T)
-        elif query_id == "6":
-            res = get_res(ts, query_id, H, T)
-        print(res)
+        while True:
+            res = get_res(t, query_id, H, T)
+            if len(res) > 0:
+                break
+            else:
+                t -= 1
         resp = Response(json.dumps(res), status=200, mimetype='application/json')
-
     except Exception as err:
         print(err)
 
